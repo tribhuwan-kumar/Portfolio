8acf1aa6b9e558708ba80f41e54a7c9a72326cc0
Tribhuwan <tribhuwankumar735275@gmail.com>
2023-10-06 23:02:38 +0530

    improvement

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,8 +1,14 @@
-// for making navbar responsive
+// Menu navbar in small devices
 
-const burgerMenu = document.querySelector('#menu');
+const burgerMenu = document.querySelector('.menu_icon');
 const navbar = document.querySelector('#navbar');
 
 burgerMenu.addEventListener('click', () => {
   navbar.classList.toggle('open');
 });
+
+const burgerIcon = document.querySelector(".menu_icon");
+    
+burgerIcon.addEventListener("click", function () {
+    burgerIcon.classList.toggle("active");
+  });
